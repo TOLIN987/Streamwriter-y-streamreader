779210deaae728f0635c84ae0f9a74cb6e02a798
pc <pc@DESKTOP-7NSEKGR>
2023-02-27 10:09:21 -0600

    Add project files.

---
+++ Streamwriter y streamreader/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Streamwriter_y_streamreader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLeer = new System.Windows.Forms.Button();
+            this.btnEscribir = new System.Windows.Forms.Button();
+            this.rtbCampo = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnLeer
+            // 
+            this.btnLeer.Location = new System.Drawing.Point(120, 42);
+            this.btnLeer.Name = "btnLeer";
+            this.btnLeer.Size = new System.Drawing.Size(129, 53);
+            this.btnLeer.TabIndex = 0;
+            this.btnLeer.Text = "Read";
+            this.btnLeer.UseVisualStyleBackColor = true;
+            this.btnLeer.Click += new System.EventHandler(this.btnLeer_Click);
+            // 
+            // btnEscribir
+            // 
+            this.btnEscribir.Location = new System.Drawing.Point(481, 42);
+            this.btnEscribir.Name = "btnEscribir";
+            this.btnEscribir.Size = new System.Drawing.Size(129, 53);
+            this.btnEscribir.TabIndex = 1;
+            this.btnEscribir.Text = "Write";
+            this.btnEscribir.UseVisualStyleBackColor = true;
+            this.btnEscribir.Click += new System.EventHandler(this.btnEscribir_Click);
+            // 
+            // rtbCampo
+            // 
+            this.rtbCampo.Location = new System.Drawing.Point(197, 151);
+            this.rtbCampo.Name = "rtbCampo";
+            this.rtbCampo.Size = new System.Drawing.Size(352, 185);
+            this.rtbCampo.TabIndex = 2;
+            this.rtbCampo.Text = "";
+            this.rtbCampo.TextChanged += new System.EventHandler(this.rtbCampo_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rtbCampo);
+            this.Controls.Add(this.btnEscribir);
+            this.Controls.Add(this.btnLeer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLeer;
+        private System.Windows.Forms.Button btnEscribir;
+        private System.Windows.Forms.RichTextBox rtbCampo;
+    }
+}
+
---
+++ Streamwriter y streamreader/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Streamwriter_y_streamreader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLeer_Click(object sender, EventArgs e)
+        {
+            using (StreamReader re = new StreamReader(@"C:\Users\pc\OneDrive\Documentos\2do semestre\Streamwriter y streamreader\Streamwriter y streamreader\TextoWR.txt"))
+            {
+                while(!re.EndOfStream)
+                {
+                    rtbCampo.AppendText(re.ReadLine());
+                }
+            }
+        }
+
+        private void btnEscribir_Click(object sender, EventArgs e)
+        {
+            using(StreamWriter wr = new StreamWriter(@"C:\Users\pc\OneDrive\Documentos\2do semestre\Streamwriter y streamreader\Streamwriter y streamreader\TextoWR.txt"))
+            {
+                wr.WriteLine((rtbCampo.Text));
+            }
+        }
+
+        private void rtbCampo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
